5b352a7ee8fac2717e647cbb5e9e05eb6afbba4f
Daniel A. White <daniel@whitefamily.in>
2019-10-27 20:35:09 -0400

    stub project

---
+++ src/MSBuild.DotNetCli/Tools/DotNetCli.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Build.Tasks;
+
+namespace MSBuild.DotNetCli.Tasks
+{
+    public class DotNetCli : ToolTaskExtension
+    {
+        protected override string ToolName => nameof(DotNetCli);
+
+        protected override string GenerateFullPathToTool()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
