d875a634d0f7f292ab354714f5835e53493dd502
Daniel A. White <daniel@whitefamily.in>
2019-11-02 16:04:35 -0400

    first implementation

---
+++ src/MSBuild.DotNetCli/Tasks/DotNetCli.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Build.Utilities;
+
+namespace DanielWhite.MSBuild.DotNetCli.Tasks
+{
+    public class DotNetCli : Task
+    {
+        public override bool Execute()
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = WorkingDirectory ?? string.Empty,
+                    FileName = "dotnet",
+                    Arguments = $"{Tool} { Arguments ?? string.Empty }",
+                    CreateNoWindow = true,
+                };
+
+                using Process process = Process.Start(processStartInfo);
+                process.WaitForExit();
+                return process.ExitCode == 0;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+
+        public string Tool { get; set; }
+
+        public string Arguments { get; set; }
+
+        public string WorkingDirectory { get; set; }
+    }
+}
